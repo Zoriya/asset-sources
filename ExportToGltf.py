b4ad686efabb204bf9c4dd6fe76cc6922fc91a1c
Shatur95 <genaloner@gmail.com>
2020-01-28 01:40:41 +0200

    Switch to Godot

---
+++ ExportToGltf.py
@@ -0,0 +1,16 @@
+import bpy
+import sys
+from pathlib import Path
+
+
+def export_file(filename, output_path):
+    bpy.ops.wm.open_mainfile(filepath=str(filename))
+    Path(output_path / filename.parent).mkdir(parents=True, exist_ok=True)
+    output_filename = str(output_path / filename.with_suffix('.glb'))
+    bpy.ops.export_scene.gltf(filepath=output_filename,
+                              export_materials=False,
+                              export_apply=True)
+
+output_path = Path(sys.argv[sys.argv.index('--') + 1])
+for filename in Path('.').glob('Objects/**/*.blend'):
+    export_file(filename, output_path)
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Asset sources
 
 Files for generating assets for Zoriya
+
+## Assets generation
+
+Script `ExportToGltf.py` automatically exports all assets in glTF to corresponding folders:
+
+```bash
+blender --background --python ExportToGltf.py -- /path/to/zoriya/
+```
