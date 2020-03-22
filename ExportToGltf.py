8a88711853f0b0bbf16100988e9882cdded36ae7
Shatur95 <genaloner@gmail.com>
2020-03-22 17:00:00 +0200

    Add landscape

--- ExportToGltf.py
+++ ExportToGltf.py
@@ -7,9 +7,7 @@ def export_file(filename, output_path):
     bpy.ops.wm.open_mainfile(filepath=str(filename))
     Path(output_path / filename.parent).mkdir(parents=True, exist_ok=True)
     output_filename = str(output_path / filename.with_suffix('.glb'))
-    bpy.ops.export_scene.gltf(filepath=output_filename,
-                              export_materials=False,
-                              export_apply=True)
+    bpy.ops.export_scene.gltf(filepath=output_filename, export_apply=True)
 
 
 def zorya_path():
@@ -20,6 +18,6 @@ def zorya_path():
 
 
 output_path = zorya_path()
-for pattern in ('Environment/Camp/*.blend', 'Items/Potions/Models/*.blend'):
+for pattern in ('Environment/Landscape/Island.blend', 'Environment/Camp/Bonfire/*.blend', 'Items/Potions/Models/*.blend'):
     for filename in Path('.').glob(pattern):
         export_file(filename, output_path)
